6c160ccbe4d19987732928bc1741eab488920514
arodest <87528874+arodest@users.noreply.github.com>
2023-01-30 20:28:32 +0100

    Update unit_test.sh

--- pipelines/PIPELINE-FULL-STAGING/unit_test.sh
+++ pipelines/PIPELINE-FULL-STAGING/unit_test.sh
@@ -6,6 +6,7 @@ export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 echo "PYTHONPATH: $PYTHONPATH"
 export DYNAMODB_TABLE=todoUnitTestsTable
 python test/unit/TestToDo.py
+python test/integration/todoApiTest.py
 pip show coverage
 coverage run --include=src/todoList.py test/unit/TestToDo.py
 coverage report -m
