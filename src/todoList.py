5f5173de7b645875dede3125752126bed12e1306
arodest <87528874+arodest@users.noreply.github.com>
2023-02-05 13:19:38 +0100

    Update todoList.py

--- src/todoList.py
+++ src/todoList.py
@@ -30,12 +30,12 @@ def get_item(key, dynamodb=None):
             }
         )
 
-    except ClientError as e:
-        print(e.response['Error']['Message'])
-    else:
-        print('Result getItem:'+str(result))
-        if 'Item' in result:
-            return result['Item']
+except ClientError as e:
+       print(e.response['Error']['Message'])
+else:
+       print('Result getItem:'+str(result))
+       if 'Item' in result:
+           return result['Item']
 
 
 def get_items(dynamodb=None):
