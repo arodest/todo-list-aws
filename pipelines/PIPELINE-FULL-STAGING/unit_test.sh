7c3e394564bcbf1542dc4287900f44345be147fc
arodest <87528874+arodest@users.noreply.github.com>
2023-02-05 13:41:53 +0100

    Update unit_test.sh

--- pipelines/PIPELINE-FULL-STAGING/unit_test.sh
+++ pipelines/PIPELINE-FULL-STAGING/unit_test.sh
@@ -9,6 +9,7 @@ python test/unit/TestToDo.py
 python test/integration/todoApiTest.py
 pip show coverage
 coverage run --include=src/todoList.py test/unit/TestToDo.py
+coverage run --include=src/create.py 
 coverage report -m
 coverage xml
 coverage html
